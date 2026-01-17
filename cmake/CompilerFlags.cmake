386549757e56c1b4b61660ac69728f0ebf33a21a
Aferim007 <minadavid007@gmail.com>
2026-01-17 19:12:15 +0200

    v0.3.1 Adaugat cerinte tema3 + rezolvat niste bug-uri

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,7 +1,5 @@
 include(cmake/CustomStdlibAndSanitizers.cmake)
 
-# target definitions
-
 function(set_compiler_flags)
     set(multiValueArgs TARGET_NAMES)
     set(oneValueArgs RUN_SANITIZERS)
@@ -11,7 +9,6 @@ function(set_compiler_flags)
         set(ARG_RUN_SANITIZERS TRUE)
     endif()
 
-    # iterate over all specified targets
     foreach (TARGET_NAME IN LISTS ARG_TARGET_NAMES)
         if(GITHUB_ACTIONS)
             message("NOTE: GITHUB_ACTIONS defined")
@@ -24,7 +21,6 @@ function(set_compiler_flags)
             set_property(TARGET ${TARGET_NAME} PROPERTY COMPILE_WARNING_AS_ERROR ON)
         endif()
 
-        # custom compiler flags
         message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION}")
         if(MSVC)
             target_compile_options(${TARGET_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
@@ -34,7 +30,6 @@ function(set_compiler_flags)
 
         ###############################################################################
 
-        # sanitizers
         if("${ARG_RUN_SANITIZERS}" STREQUAL "TRUE")
             if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
             else()
